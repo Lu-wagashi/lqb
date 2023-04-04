d8c6310fec4f0c0839c38663aeb7ad28c14be64e
Jones <72455267+Lu-wagashi@users.noreply.github.com>
2023-04-05 04:24:04 +0800

    Update 子串分值和-暴力过40%.cpp

--- 2020届/子串分值和-暴力过40%.cpp
+++ 2020届/子串分值和-暴力过40%.cpp
@@ -1,25 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-int n,a[1005],b[1005];
-
-
-int main()
-{
-	scanf("%d",&n);
-	for(int i=1;i<=n;i++)
-	{
-		scanf("%d",&a[i]);
-		scanf("%d",&b[i]);
+ 
+int count(string ss) {
+	set<char> sset;
+	int cnt = 0;
+	for (int i = 0; i < ss.size(); i++) {
+		if (sset.count(ss[i]) == 0) {	//统计ss[i]在sset中出现的次数，==0代表没出现过 
+			sset.insert(ss[i]);	//将ss[i]插入sset容器中， 
+		}
 	}
-	if(n==1) cout<<"2";
-	if(n==2) 
-	{
-		if(a[1]==b[1]  && a[2]==b[2]) cout<<"2";
-		if(a[1]==b[1]) cout<<"3";//平行为对应到a1=b1 
-		cout<<"4";
-	 } 
-	if(n==3) cout<<"6";//这只是示例，具体的n==3，有7种情况
-	if(n>3) cout<<2*n;
+	cnt=sset.size();				//统计只出现过一次的字符数 
+	return cnt;
+}
+ 
+int main() {
+	string S, sub;
+	cin >> S;//存入字符串 
+	long long ans = 0;
+	int len = S.size();//字符串长度 
+	for (int i = 0; i < len; i++) {//穷举各个字符 
+		for (int j = i; j < len; j++) {//枚举从循环字符位置i之后的字符 
+			sub = S.substr(i, (j-i+1));  // 返回从pos号位i开始、长度为len的子串sub，
+			ans += count(sub);		//统计这一子串sub的非重复数字 
+		}
+	}
+	cout << ans;
 	return 0;
 }
